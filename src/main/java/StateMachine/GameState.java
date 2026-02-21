86d074a76daaaa374e9ad7a59aa091a53948105e
sayaka04 <d.franco.536439@umindanao.edu.ph>
2026-02-21 22:01:40 +0800

    adding toggleable In-Game-Menu tab

--- src/main/java/StateMachine/GameState.java
+++ src/main/java/StateMachine/GameState.java
@@ -8,10 +8,17 @@ import org.lwjgl.opengl.GL11;
 public class GameState extends State {
 
     private TextRenderer buttonTextFont; // Add this!
-    private Button newGameButton; // Add this!
-    private Button loadGameButton; // Add this!
-    private Button settingButton; // Add this!
-    private Button exitGameButton; // Add this!
+    private Button option1Button; // Add this!
+    private Button option2Button; // Add this!
+    private Button option3Button; // Add this!
+    private Button option4Button; // Add this!
+
+
+    private boolean tabIsVisible = false;
+    private Button tabMenuButton; // Add this!
+    private Button tabSaveButton; // Add this!
+    private Button tabLoadButton; // Add this!
+    private Button tabExitButton; // Add this!
 
     private BackgroundMusic backgroundMusic;
     private Sound hoverSound;
@@ -36,10 +43,15 @@ public class GameState extends State {
         clickSound = new Sound("C:\\Users\\Acer\\OneDrive\\Desktop\\edited683098__florianreichelt__click.ogg");
 
         // Init Button: create a button at X: 500, Y: 300. Width: 200, Heisght: 100.
-        newGameButton = new Button((1920/2) - 250, 250, 500, 50, hoverSound, "Option 1", buttonTextFont);
-        loadGameButton = new Button((1920/2) - 250, 320, 500, 50, hoverSound, "Option 2", buttonTextFont);
-        settingButton = new Button((1920/2) - 250, 390, 500, 50, hoverSound, "Option 3", buttonTextFont);
-        exitGameButton = new Button((1920/2) - 250, 460, 500, 50, hoverSound, "Option 4", buttonTextFont);
+        option1Button = new Button((1920/2) - 250, 250, 500, 50, hoverSound, "Option 1", buttonTextFont);
+        option2Button = new Button((1920/2) - 250, 320, 500, 50, hoverSound, "Option 2", buttonTextFont);
+        option3Button = new Button((1920/2) - 250, 390, 500, 50, hoverSound, "Option 3", buttonTextFont);
+        option4Button = new Button((1920/2) - 250, 460, 500, 50, hoverSound, "Option 4", buttonTextFont);
+
+        tabMenuButton = new Button(0, 0, 150, 50, hoverSound, "Menu", buttonTextFont);
+        tabSaveButton = new Button(150, 0, 150, 50, hoverSound, "Save", buttonTextFont);
+        tabLoadButton = new Button(300, 0, 150, 50, hoverSound, "Load", buttonTextFont);
+        tabExitButton = new Button(450, 0, 150, 50, hoverSound, "Exit", buttonTextFont);
 
         dialogue = new Dialogue(0, 780, 1920, 300, hoverSound, "C:\\Users\\Acer\\OneDrive\\Desktop\\Jersey10-Regular.ttf", 48);
 
@@ -63,17 +75,24 @@ public class GameState extends State {
         renderer.drawImage(character, 0, 0, 1920, 1080);
 
 
-        if (newGameButton.isVisible) {
-            newGameButton.draw(window.getMouseX(), window.getMouseY());
+        if (tabIsVisible) {
+            tabMenuButton.draw(window.getMouseX(), window.getMouseY());
+            tabSaveButton.draw(window.getMouseX(), window.getMouseY());
+            tabLoadButton.draw(window.getMouseX(), window.getMouseY());
+            tabExitButton.draw(window.getMouseX(), window.getMouseY());
         }
-        if (loadGameButton.isVisible) {
-            loadGameButton.draw(window.getMouseX(), window.getMouseY());
+
+        if (option1Button.isVisible) {
+            option1Button.draw(window.getMouseX(), window.getMouseY());
         }
-        if (settingButton.isVisible) {
-            settingButton.draw(window.getMouseX(), window.getMouseY());
+        if (option2Button.isVisible) {
+            option2Button.draw(window.getMouseX(), window.getMouseY());
         }
-        if (exitGameButton.isVisible) {
-            exitGameButton.draw(window.getMouseX(), window.getMouseY());
+        if (option3Button.isVisible) {
+            option3Button.draw(window.getMouseX(), window.getMouseY());
+        }
+        if (option4Button.isVisible) {
+            option4Button.draw(window.getMouseX(), window.getMouseY());
         }
 
         if (dialogue.isVisible) {
@@ -90,7 +109,31 @@ public class GameState extends State {
 
     @Override
     public void mouseClickHandler(Window window){
-        if (newGameButton.checkClick(window.getMouseX(), window.getMouseY())) {
+
+        if (tabIsVisible) {
+            if (tabMenuButton.checkClick(window.getMouseX(), window.getMouseY())) {
+                clickSound.play();
+                backgroundMusic.stop();
+                State.current = State.menu;
+                State.current.enter();
+            }
+            else if (tabSaveButton.checkClick(window.getMouseX(), window.getMouseY())) {
+                clickSound.play();
+                System.out.println("Save Clicked!");
+            }
+            else if (tabLoadButton.checkClick(window.getMouseX(), window.getMouseY())) {
+                clickSound.play();
+                System.out.println("Save Clicked!");
+            }
+            else if (tabExitButton.checkClick(window.getMouseX(), window.getMouseY())) {
+                clickSound.play();
+                System.out.println("Exit pressed! Closing the game...");
+                GLFW.glfwSetWindowShouldClose(window.glfwWindow, true);   // This tells the while loop to stop, safely closing the game
+            }
+        }
+
+
+        if (option1Button.checkClick(window.getMouseX(), window.getMouseY())) {
 
             System.out.println("Button Clicked! Hiding button now...");
             backgroundMusic.stop();
@@ -103,17 +146,17 @@ public class GameState extends State {
             // TURN THE BUTTON OFF!
 //            newGameButton.isVisible = false;    // This stops it from drawing AND stops the ghost clicks!
         }
-        else if (loadGameButton.checkClick(window.getMouseX(), window.getMouseY())) {
+        else if (option2Button.checkClick(window.getMouseX(), window.getMouseY())) {
             System.out.println("Button Clicked! Yay so simple to use hahaha...");
             clickSound.play();
             // State.current = State.game;
             // State.current.enter();
         }
-        else if (settingButton.checkClick(window.getMouseX(), window.getMouseY())) {
+        else if (option3Button.checkClick(window.getMouseX(), window.getMouseY())) {
             System.out.println("Button Clicked! Yay so simple to use hahaha...");
             clickSound.play();
         }
-        else if (exitGameButton.checkClick(window.getMouseX(), window.getMouseY())) {
+        else if (option4Button.checkClick(window.getMouseX(), window.getMouseY())) {
             System.out.println("Button Clicked! Yay so simple to use hahaha...");
             clickSound.play();
         }
@@ -127,9 +170,15 @@ public class GameState extends State {
             dialogue.setDialogueText("Spacebar");
         }
         else if (key == GLFW.GLFW_KEY_ESCAPE && action == GLFW.GLFW_PRESS) {
-            System.out.println("Escape pressed!");
+            if(tabIsVisible){
+                tabIsVisible = false;
+            }
+            else{
+                tabIsVisible = true;
+            }
+            System.out.println("Escape pressed! toggle: " + tabIsVisible);
             dialogue.setName("ESC");
-            dialogue.setDialogueText("ESC");
+            dialogue.setDialogueText("Escape pressed");
         }
         else if (key == GLFW.GLFW_KEY_A && action == GLFW.GLFW_PRESS) {
             System.out.println("The 'A' key was pressed!");
